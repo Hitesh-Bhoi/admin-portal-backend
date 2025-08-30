361e837e8efda30b4026ac72d11b6421855447a0
bhoi hitesh <bhoihitesh1492@gmail.com>
2025-08-30 21:14:17 +0530

    updated the email template to add website logo

--- utils/mailTemplate.js
+++ utils/mailTemplate.js
@@ -2,11 +2,21 @@ function resetPasswordTemplate({ userInfo, token, expiresIn }) {
   return `
     <!doctype html>
     <html>
-      <body style="font-family: Arial, sans-serif; background:#f4f6f8; padding:20px;">
+      <body style="font-family: Arial, sans-serif; background:#f4f6f8; padding:20px; text-align: center;">
         <div style="max-width:600px; margin:auto; background:#fff; padding:24px; border-radius:8px; border:1px solid #e5e7eb;">
+          <img src="https://admin-portal-topaz.vercel.app/icons/brand/gn.png" 
+          alt="Brand Logo" 
+          width="150" 
+          style="display:block; margin:auto;" />
           <h2 style="color:#111827;">Reset your password</h2>
-          <p>Hi ${userInfo.fullname}, we received a request to reset the password for your account.</p>
-          <p>Click the button below to reset your password. This link is valid for <b>${expiresIn}</b>.</p>
+          <h3 style="color:#111827;">Hi ${userInfo.fullname}</h3>
+          <p style="color:#111827; font-size:14px; line-height:20px;">
+          we received a request to reset the password for your account.
+        </p>
+        <p style="color:#111827; font-size:14px; line-height:20px;">
+        Click the button below to reset your password, This link is valid for 
+        <b>${expiresIn}</b>.
+        </p>
           <p style="text-align:center; margin:24px 0;">
             <a href="http://localhost:3000/reset-password?token=${token}" style="background:#2563eb; color:white; padding:12px 20px; border-radius:6px; text-decoration:none; font-weight:600;">
               Reset Password
